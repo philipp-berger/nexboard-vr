056d1a87bd286e54b1adefb53bda6ffc61eee0a4
Markus Petrykowski <markus.petrykowski@nexenio.com>
2017-11-19 22:23:31 +0100

    add working room

--- src/main.jsx
+++ src/main.jsx
@@ -77,7 +77,6 @@ export default class App extends React.Component {
   }
 
   onPositionChange(newPosition) {
-    console.log("Position Changed", newPosition)
     this.setState({
       position: {
         x: newPosition.x,
@@ -90,13 +89,15 @@ export default class App extends React.Component {
   
 
   render () {
+    // <a-box color="tomato" depth="100" height="0.1" width="100" position="0 -1 0"></a-box>
+    // <a-box color="green" depth="1" height="1" width="1" position="1 0 -1"></a-box>
     return (
       <Entity>
-        <a-box color="tomato" depth="100" height="0.1" width="100" position="0 -1 0"></a-box>
-        <a-box color="green" depth="1" height="1" width="1" position="1 0 -1"></a-box>
         <a-entity gltf-model="url(/head.gltf)" scale='0.0005 0.0005 0.0005' 
           position={`${this.state.position.x} ${this.state.position.y} ${this.state.position.z}`} 
           rotation={`${this.state.rotation.x} ${this.state.rotation.y} ${this.state.rotation.z}`}>
+        </a-entity>
+        <a-entity gltf-model="url(/scene.gltf)" position="-14 -2.5 7" scale='0.05 0.05 0.05'>
         </a-entity>
 
         <Camera y={-1.5} 
